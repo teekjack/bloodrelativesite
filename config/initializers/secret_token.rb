16cf47dd90c6b4df67520a5962d56ac6d583f1bc
Pratik Mukerji <pmukerji@ethz.ch>
2012-04-15 21:59:04 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloodrelative::Application.config.secret_token = 'a9800ec3d11665b2e068cc54c8457ce4b108320fb104744c15e59898c9afd4a371b9f71c06f7163ec3e775b4a696b58e726d405f8dfeb287733cb597d6b1b2c0'
